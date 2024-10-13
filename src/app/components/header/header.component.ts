be947a8538e0e436f9d7f9bf79155b5a33e5a63e
Vinicius Sousa <csousa.vinicius@gmail.com>
2024-10-13 15:05:45 -0300

    new: header fixes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(private _router: Router) { }
-
-  navigateToCreateNote() {
-    this._router.navigate(['/text-notes/create']);
-  }
+  constructor() { }
 }
